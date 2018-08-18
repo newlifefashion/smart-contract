cd0c3483c154a30858145c4cb61b9b6052af1b5e
deadlock <mhfred.deadlock@gmail.com>
2018-08-18 16:00:29 +0800

    v0.0.1
    remove unused functions

--- contracts/token/token.vesting.hpp
+++ contracts/token/token.vesting.hpp
@@ -35,18 +35,15 @@ namespace token {
                       asset quantity,
                       string memo);
 
-
-        inline asset get_supply(symbol_name sym) const;
-
-        inline asset get_balance(account_name owner, symbol_name sym) const;
-
     private:
         //@abi table accounts i64
         struct account {
             asset balance;
 
             uint64_t primary_key() const { return balance.symbol.name(); }
-            EOSLIB_SERIALIZE( account, (balance) )
+
+            EOSLIB_SERIALIZE( account, (balance)
+            )
         };
 
         //@abi table stat i64
@@ -56,7 +53,9 @@ namespace token {
             account_name issuer;
 
             uint64_t primary_key() const { return supply.symbol.name(); }
-            EOSLIB_SERIALIZE( stat, (supply)(max_supply)(issuer) )
+
+            EOSLIB_SERIALIZE( stat, (supply)(max_supply)(issuer)
+            )
         };
 
         typedef eosio::multi_index<N(accounts), account> accounts;
@@ -65,29 +64,10 @@ namespace token {
         void sub_balance(account_name owner, asset value);
 
         void add_balance(account_name owner, asset value, account_name ram_payer);
-
-    public:
-        struct transfer_args {
-            account_name from;
-            account_name to;
-            asset quantity;
-            string memo;
-        };
     };
-
-    asset vesting::get_supply(symbol_name sym) const {
-        stats statstable(_self, sym);
-        const auto &st = statstable.get(sym);
-        return st.supply;
-    }
-
-    asset vesting::get_balance(account_name owner, symbol_name sym) const {
-        accounts accountstable(_self, owner);
-        const auto &ac = accountstable.get(sym);
-        return ac.balance;
-    }
 
 }
 
-EOSIO_ABI(token::vesting, (create)(issue)(transfer))
+EOSIO_ABI(token::vesting, (create)(issue)
+(transfer))
 
